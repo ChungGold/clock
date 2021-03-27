7ccdc3f539702291979505ee0e1c5a4e199bbbd4
ChungGold <chungau@csu.fullerton.edu>
2021-03-27 00:16:00 -0700

    switch statement to accomodate time of day

--- src/clock.jsx
+++ src/clock.jsx
@@ -12,9 +12,18 @@ function Clock() {
 
     setInterval(() => {
         let time = new Date().toLocaleTimeString().replace(/\D/g,'').split('').map(Number);
-        setHour(300+time[1]*30+time[2]+time[3]*0.5);
-        setMinute((time[2]*10+time[3])*6);
-        setSecond((time[4]*10+time[5])*6);
+        
+        switch (time.length){
+            case 5:
+                setHour(time[0]*30+time[1]+time[2]*0.5);
+                setMinute((time[1]*10+time[2])*6);
+                setSecond((time[3]*10+time[4])*6);
+                break;
+            case 6:
+                setHour(300+time[1]*30+time[2]+time[3]*0.5);
+                setMinute((time[2]*10+time[3])*6);
+                setSecond((time[4]*10+time[5])*6);
+        }
     }, 1000);
 
     const hourHand = {
